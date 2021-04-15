a9556acb4d3ebd1c1550bb437c6ce80370f11add
ht-chchoi <chchoi@hdtel.co.kr>
2021-04-15 14:31:37 +0900

    Initial commit

---
+++ settings.gradle.kts
@@ -0,0 +1,1 @@
+rootProject.name = "ceremony_light_v2"
---
+++ src/main/kotlin/com/ht/ceremony_light_v2/CeremonyLightV2Application.kt
@@ -0,0 +1,11 @@
+package com.ht.ceremony_light_v2
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class CeremonyLightV2Application
+
+fun main(args: Array<String>) {
+    runApplication<CeremonyLightV2Application>(*args)
+}
